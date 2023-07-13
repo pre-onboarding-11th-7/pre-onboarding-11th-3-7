5ae3fbb77fcce8d028ca55a8eb6f83f8d735a1e6
dawnheee <sherlock130815@gmail.com>
2023-07-14 00:59:58 +0900

    [feat] Context Provider 위치 index->App 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { ListProvider } from "./context/listContext";
-import { ListService } from "./api/ListService";
-import { HttpClient } from "./api/httpClient/httpClient";
-import { PageNumProvider } from "./context/pageNumContext";
-
-const httpClient = new HttpClient(
-  process.env.REACT_APP_BASE_URL || "default-value",
-  process.env.REACT_APP_GITHUB_TOKEN || "default-value"
-);
-
-const listService = new ListService(httpClient);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <PageNumProvider>
-    <ListProvider listService={listService}>
-      <App />
-    </ListProvider>
-  </PageNumProvider>
-);
+root.render(<App />);
