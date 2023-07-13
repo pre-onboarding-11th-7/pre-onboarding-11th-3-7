d59008e13e2550fb1fa1a67d2d189d5391a72171
dawnheee <sherlock130815@gmail.com>
2023-07-14 01:11:34 +0900

    [feat] context Provider 수정

--- src/api/IssueService.tsx
+++ src/api/IssueService.tsx
@@ -1,16 +1,24 @@
 import { HttpClient } from "./httpClient/httpClient";
 import { Issue } from "../@types/types";
 
-export class ListService {
+export class IssueService {
   private httpClient: HttpClient;
 
   constructor(httpClient: HttpClient) {
     this.httpClient = httpClient;
   }
 
-  async get(pageNum?: number) {
+  async getList(pageNum?: number) {
     const { data }: { data: Issue[] } = await this.httpClient.request(
       `/repos/facebook/react/issues?state=open&sort=comments&per_page=10&page=${pageNum}`,
+      "GET"
+    );
+    return data;
+  }
+
+  async getDetail(issueNum?: number) {
+    const data = await this.httpClient.request(
+      `/repos/facebook/react/issues/${issueNum}`,
       "GET"
     );
     return data;
