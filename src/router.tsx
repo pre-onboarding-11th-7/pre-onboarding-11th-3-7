6698e81752b0e6529332ce4db24e03ad10ce2f04
mini <0miiii@naver.com>
2023-07-14 14:19:38 +0900

    feat: #3 라우터에 루트레이아웃 적용
    두 페이지는 공통 헤더를 공유합니다.

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { RootLayout } from "./components";
+import { DetailPage, HomePage } from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/:num",
+        element: <DetailPage />,
+      },
+    ],
   },
 ]);
 
